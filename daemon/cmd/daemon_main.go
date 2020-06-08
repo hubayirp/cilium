hubayirp/cilium

commit c3b5ca6fdc4085d88a5916e74910ae7b5a955da5

add support for k8s endpoint slice
This PR adds support for k8s endpoint slice. Since kubernetes does not
enable this feature by default, Cilium will detect if the feature is
enabled by checking if there is at least a single endpoint slice
available in the whole cluster. The user can always fallback to
v1.Endpoints by setting the option
`enable-k8s-endpoint-slice-auto-detection` to false which makes Cilium
to only watch for v1.Endpoint k8s events.

Signed-off-by: André Martins <andre@cilium.io>

pkg/k8s/watchers/watcher.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -58,10 +58,12 @@ const (
 	k8sAPIGroupCiliumNodeV2                     = "cilium/v2::CiliumNode"
 	k8sAPIGroupCiliumEndpointV2                 = "cilium/v2::CiliumEndpoint"
 	cacheSyncTimeout                            = 3 * time.Minute
+	K8sAPIGroupEndpointSliceV1Beta1Discovery    = "discovery/v1beta1::EndpointSlice"
 
 	metricCNP            = "CiliumNetworkPolicy"
 	metricCCNP           = "CiliumClusterwideNetworkPolicy"
 	metricEndpoint       = "Endpoint"
+	metricEndpointSlice  = "EndpointSlice"
 	metricKNP            = "NetworkPolicy"
 	metricNS             = "Namespace"
 	metricCiliumNode     = "CiliumNode"
@@ -411,7 +413,19 @@ func (k *K8sWatcher) EnableK8sWatcher(queueSize uint) error {
 	// kubernetes endpoints
 	serEps := serializer.NewFunctionQueue(queueSize)
 	swgEps := lock.NewStoppableWaitGroup()
-	k.endpointsInit(k8s.Client(), serEps, swgEps)
+
+	// We only enable either "Endpoints" or "EndpointSlice"
+	switch {
+	case k8s.SupportsEndpointSlice():
+		connected := k.endpointSlicesInit(k8s.Client(), serEps, swgEps)
+		// the cluster has endpoint slices so we should not check for v1.Endpoints
+		if connected {
+			break
+		}
+		fallthrough
+	default:
+		k.endpointsInit(k8s.Client(), serEps, swgEps)
+	}
 
 	// cilium network policies
 	serCNPs := serializer.NewFunctionQueue(queueSize)

daemon/daemon_main.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Authors of Cilium
+// Copyright 2016-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -334,6 +334,9 @@ func init() {
 
 	flags.Bool(option.EnableK8sExternalIPs, defaults.EnableK8sExternalIPs, fmt.Sprintf("Enable k8s externalIPs feature (Only enabled in conjunction with %s)", option.EnableNodePort))
 	option.BindEnv(option.EnableK8sExternalIPs)
+
+	flags.Bool(option.K8sEnableEndpointSlice, defaults.K8sEnableEndpointSlice, "Enables k8s EndpointSlice feature in Cilium if the k8s cluster supports it")
+	option.BindEnv(option.K8sEnableEndpointSlice)
 
 	flags.Bool(option.EnableL7Proxy, defaults.EnableL7Proxy, "Enable L7 proxy for L7 policy enforcement")
 	option.BindEnv(option.EnableL7Proxy)

install/kubernetes/cilium/charts/agent/templates/clusterrole.yaml
@@ -12,6 +12,14 @@ rules:
   - list
   - watch
 - apiGroups:
+  - discovery.k8s.io
+  resources:
+  - endpointslices
+  verbs:
+  - get
+  - list
+  - watch
+- apiGroups:
   - ""
   resources:
   - namespaces

install/kubernetes/cilium/charts/operator/templates/clusterrole.yaml
@@ -15,6 +15,14 @@ rules:
   - watch
   - delete
 - apiGroups:
+  - discovery.k8s.io
+  resources:
+  - endpointslices
+  verbs:
+  - get
+  - list
+  - watch
+- apiGroups:
   - ""
   resources:
   # to automatically read from k8s and import the node's pod CIDR to cilium's

install/kubernetes/quick-install.yaml
@@ -154,6 +154,14 @@ rules:
   - list
   - watch
 - apiGroups:
+  - discovery.k8s.io
+  resources:
+  - endpointslices
+  verbs:
+  - get
+  - list
+  - watch
+- apiGroups:
   - ""
   resources:
   - namespaces
@@ -224,6 +232,14 @@ rules:
   - list
   - watch
   - delete
+- apiGroups:
+  - discovery.k8s.io
+  resources:
+  - endpointslices
+  verbs:
+  - get
+  - list
+  - watch
 - apiGroups:
   - ""
   resources:

operator/k8s_service_sync.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Authors of Cilium
+// Copyright 2018-2020 Authors of Cilium
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,11 @@ package main
 
 import (
 	"context"
+	"sync"
 	"time"
+
+	"k8s.io/api/discovery/v1beta1"
+	"k8s.io/client-go/kubernetes"
 
 	"github.com/cilium/cilium/pkg/k8s"
 	"github.com/cilium/cilium/pkg/k8s/informer"
@@ -178,9 +182,47 @@ func startSynchronizingServices() {
 
 	go svcController.Run(wait.NeverStop)
 
+	var (
+		endpointController cache.Controller
+	)
+
+	// We only enable either "Endpoints" or "EndpointSlice"
+	switch {
+	case k8s.SupportsEndpointSlice():
+		var endpointSliceEnabled bool
+		endpointController, endpointSliceEnabled = endpointSlicesInit(k8s.Client(), serEps, swgEps)
+		// the cluster has endpoint slices so we should not check for v1.Endpoints
+		if endpointSliceEnabled {
+			break
+		}
+		fallthrough
+	default:
+		endpointController = endpointsInit(k8s.Client(), serEps, swgEps)
+		go endpointController.Run(wait.NeverStop)
+	}
+
+	go func() {
+		cache.WaitForCacheSync(wait.NeverStop, svcController.HasSynced)
+		swgSvcs.Stop()
+		cache.WaitForCacheSync(wait.NeverStop, endpointController.HasSynced)
+		swgEps.Stop()
+
+		swgSvcs.Wait()
+		swgEps.Wait()
+		close(k8sSvcCacheSynced)
+	}()
+
+	go func() {
+		<-readyChan
+		log.Info("Starting to synchronize Kubernetes services to kvstore")
+		k8sServiceHandler()
+	}()
+}
+
+func endpointsInit(k8sClient kubernetes.Interface, serEps *serializer.FunctionQueue, swgEps *lock.StoppableWaitGroup) cache.Controller {
 	// Watch for v1.Endpoints changes and push changes into ServiceCache
 	_, endpointController := informer.NewInformer(
-		cache.NewListWatchFromClient(k8s.Client().CoreV1().RESTClient(),
+		cache.NewListWatchFromClient(k8sClient.CoreV1().RESTClient(),
 			"endpoints", v1.NamespaceAll,
 			// Don't get any events from kubernetes endpoints.
 			fields.ParseSelectorOrDie("metadata.name!=kube-scheduler,metadata.name!=kube-controller-manager"),
@@ -241,23 +283,89 @@ func startSynchronizingServices() {
 		},
 		k8s.ConvertToK8sEndpoints,
 	)
+	return endpointController
+}
 
-	go endpointController.Run(wait.NeverStop)
+func endpointSlicesInit(k8sClient kubernetes.Interface, serEps *serializer.FunctionQueue, swgEps *lock.StoppableWaitGroup) (cache.Controller, bool) {
+	var (
+		hasEndpointSlices = make(chan struct{})
+		once              sync.Once
+	)
 
-	go func() {
-		cache.WaitForCacheSync(wait.NeverStop, svcController.HasSynced)
-		swgSvcs.Stop()
-		cache.WaitForCacheSync(wait.NeverStop, endpointController.HasSynced)
-		swgEps.Stop()
+	_, endpointController := informer.NewInformer(
+		cache.NewListWatchFromClient(k8sClient.DiscoveryV1beta1().RESTClient(),
+			"endpointslices", v1.NamespaceAll, fields.Everything()),
+		&v1beta1.EndpointSlice{},
+		0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				once.Do(func() {
+					// signalize that we have received an endpoint slice
+					// so it means the cluster has endpoint slices enabled.
+					close(hasEndpointSlices)
+				})
+				metrics.EventTSK8s.SetToCurrentTime()
+				if k8sEP := k8s.CopyObjToV1EndpointSlice(obj); k8sEP != nil {
+					swgEps.Add()
+					serEps.Enqueue(func() error {
+						defer swgEps.Done()
+						k8sSvcCache.UpdateEndpointSlices(k8sEP, swgEps)
+						return nil
+					}, serializer.NoRetry)
+				}
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				metrics.EventTSK8s.SetToCurrentTime()
+				if oldk8sEP := k8s.CopyObjToV1EndpointSlice(oldObj); oldk8sEP != nil {
+					if newk8sEP := k8s.CopyObjToV1EndpointSlice(newObj); newk8sEP != nil {
+						if k8s.EqualV1EndpointSlice(oldk8sEP, newk8sEP) {
+							return
+						}
+						swgEps.Add()
+						serEps.Enqueue(func() error {
+							defer swgEps.Done()
+							k8sSvcCache.UpdateEndpointSlices(newk8sEP, swgEps)
+							return nil
+						}, serializer.NoRetry)
+					}
+				}
+			},
+			DeleteFunc: func(obj interface{}) {
+				metrics.EventTSK8s.SetToCurrentTime()
+				k8sEP := k8s.CopyObjToV1EndpointSlice(obj)
+				if k8sEP == nil {
+					deletedObj, ok := obj.(cache.DeletedFinalStateUnknown)
+					if !ok {
+						return
+					}
+					// Delete was not observed by the watcher but is
+					// removed from kube-apiserver. This is the last
+					// known state and the object no longer exists.
+					k8sEP = k8s.CopyObjToV1EndpointSlice(deletedObj.Obj)
+					if k8sEP == nil {
+						return
+					}
+				}
+				swgEps.Add()
+				serEps.Enqueue(func() error {
+					defer swgEps.Done()
+					k8sSvcCache.DeleteEndpointSlices(k8sEP, swgEps)
+					return nil
+				}, serializer.NoRetry)
+			},
+		},
+		k8s.ConvertToK8sEndpointSlice,
+	)
+	ecr := make(chan struct{})
+	go endpointController.Run(ecr)
 
-		swgSvcs.Wait()
-		swgEps.Wait()
-		close(k8sSvcCacheSynced)
-	}()
+	if k8s.HasEndpointSlice(hasEndpointSlices, endpointController) {
+		return endpointController, true
+	}
 
-	go func() {
-		<-readyChan
-		log.Info("Starting to synchronize Kubernetes services to kvstore")
-		k8sServiceHandler()
-	}()
+	// K8s is not running with endpoint slices enabled, stop the endpoint slice
+	// controller to avoid watching for unnecessary stuff in k8s.
+	close(ecr)
+
+	return nil, false
 }

operator/main.go
@@ -134,6 +134,8 @@ func init() {
 	flags.Int64Var(&eniParallelWorkers, "eni-parallel-workers", defaults.ENIParallelWorkers, "Maximum number of parallel workers used by ENI allocator")
 	flags.String(option.K8sNamespaceName, "", "Name of the Kubernetes namespace in which Cilium Operator is deployed in")
 	option.BindEnv(option.K8sNamespaceName)
+	flags.Bool(option.K8sEnableEndpointSlice, defaults.K8sEnableEndpointSlice, fmt.Sprintf("Enables k8s EndpointSlice feature into Cilium-Operator if the k8s cluster supports it"))
+	option.BindEnv(option.K8sEnableEndpointSlice)
 
 	flags.IntVar(&unmanagedKubeDnsWatcherInterval, "unmanaged-pod-watcher-interval", 15, "Interval to check for unmanaged kube-dns pods (0 to disable)")
 

pkg/defaults/defaults.go
@@ -135,6 +135,9 @@ const (
 	// EnableK8sExternalIPs is the default value for k8s externalIPs feature.
 	EnableK8sExternalIPs = true
 
+	// K8sEnableEndpointSlice is the default value for k8s EndpointSlice feature.
+	K8sEnableEndpointSlice = true
+
 	// PreAllocateMaps is the default value for BPF map preallocation
 	PreAllocateMaps = true
 

commit e3593a276bb0324a612b1bb6a0f8aafb2986baaf

pkg/k8s: wait for EndpointSlice to be ready before initializing Cilium
Similar to the v1.Endpoints, in case Cilium is running with
EndpointSlice enabled, it should wait for the k8s watchers watching
those resources before it starts.

Fixes: c3b5ca6fdc40 ("add support for k8s endpoint slice")
Signed-off-by: André Martins <andre@cilium.io>

daemon/cmd/daemon_main.go
@@ -1223,7 +1223,11 @@ func (d *Daemon) initKVStore() {
 		// up etcd so we can perform the name resolution for etcd-operator
 		// to the service IP as well perform the service -> backend IPs for
 		// that service IP.
-		d.k8sWatcher.WaitForCacheSync(watchers.K8sAPIGroupServiceV1Core, watchers.K8sAPIGroupEndpointV1Core)
+		d.k8sWatcher.WaitForCacheSync(
+			watchers.K8sAPIGroupServiceV1Core,
+			watchers.K8sAPIGroupEndpointV1Core,
+			watchers.K8sAPIGroupEndpointSliceV1Beta1Discovery,
+		)
 		log := log.WithField(logfields.LogSubsys, "etcd")
 		goopts.DialOption = []grpc.DialOption{
 			grpc.WithDialer(k8s.CreateCustomDialer(&d.k8sWatcher.K8sSvcCache, log)),

pkg/k8s/watchers/watcher.go
@@ -370,6 +370,7 @@ func (k *K8sWatcher) InitK8sSubsystem() <-chan struct{} {
 			// To perform the service translation and have the BPF LB datapath
 			// with the right service -> backend (k8s endpoints) translation.
 			K8sAPIGroupEndpointV1Core,
+			K8sAPIGroupEndpointSliceV1Beta1Discovery,
 			// We need all network policies in place before restoring to make sure
 			// we are enforcing the correct policies for each endpoint before
 			// restarting.